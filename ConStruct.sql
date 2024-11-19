fa35a463f184dd13d58c1e1b91caa646b6c4aecc
Ashley Fall 24 <ac135@wellesley.edu>
2024-11-19 11:06:41 -0500

    changed ddl

--- ConStruct.sql
+++ ConStruct.sql
@@ -1,6 +1,6 @@
 use construct_db;
 -- Fangzhong Xu and Ashley Cong
-drop table if exists company;
+drop table if exists companies;
 drop table if exists attendees;
 drop table if exists events;
 drop table if exists users;
@@ -8,9 +8,9 @@ drop table if exists users;
 
 CREATE TABLE users  ( 
 
-    uid  int, 
-    name  varchar(40), 
-    phnum  int,
+    uid int, 
+    name varchar(40), 
+    phnum int,
     email varchar(40),
     password varchar(30),
     cid int,
@@ -23,44 +23,44 @@ CREATE TABLE events  (
 
     eid  int, 
     title varchar(60), 
-    desc varchar(1000),
-    host int, --is uid--[ref: > user.uid]
+    descript varchar(1000),
+    host int, -- is uid--[ref: > user.uid]
     industry enum('Academic','Energy',  'Materials', 'Industrials',  'Consumer Discretionary/Staples', 'Health Care', 
      'Financials', 'Information Technology', 'Real Estate',  'Communication Services', 'Utilities','Other') ,
     location varchar(100),
-    start_date datetime, --check if format fits
+    start_date datetime, -- check if format fits
     end_date datetime,
 
     PRIMARY Key (eid),
     index (title),
-    foreign key (host) references user(uid)
+    foreign key (host) references users(uid)
         on update restrict
-        on delete restrict --cannot delete this user if the user is hosting an event
+        on delete restrict -- cannot delete this user if the user is hosting an event
 )
 ENGINE = InnoDB;
 
 CREATE TABLE attendees(
     eid int,
     aid int, -- attendee
-    checked_in bool,
-    foreign key (eid) references event(eid)
+    checked_in boolean,
+    foreign key (eid) references events(eid)
         on update restrict
         on delete restrict
-    foreign key (attendee) references user(uid)
+    foreign key (aid) references users(uid)
         on update cascade
         on delete cascade
 )
 ENGINE = InnoDB;
 
-CREATE TABLE companys(
+CREATE TABLE companies(
     cid int,
     name varchar(100),
     
     PRIMARY KEY (cid),
 
-    foreign key (cid) references user(cid)
-        on update cascade
-        on delete cascade
+    foreign key (cid) references users(cid)
+        on update restrict
+        on delete restrict
 
 )
 ENGINE = InnoDB;
